2ddcbaba49b0604b884a7900a3475a3b44c39cb6
luoyuxiang61 <luoyuxiangqop@gmail.com>
2017-11-27 11:35:41 +0800

    sql server

--- index.js
+++ index.js
@@ -1,6 +1,63 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 var app = express();
+var Connection = require('tedious').Connection;
+var config = {
+  userName:'sa',
+  password:'Luoyuxiang61.',
+  server:'localhost',
+  options:{database:'Silverlight_NETDA'} 
+}
+var connection = new Connection(config);
+connection.on('connect',function(err){
+  if(err) console.log('err!');
+  console.log("connected");
+  executeStatement();
+})
+
+var Request = require('tedious').Request;  
+var TYPES = require('tedious').TYPES;  
+
+function executeStatement() {  
+    request = new Request("SELECT * FROM  dbo.BaseDictionary;", function(err) {  
+    if (err) {  
+        console.log(err);}  
+    });  
+    var result = "";  
+    request.on('row', function(columns) {  
+        columns.forEach(function(column) {  
+          if (column.value === null) {  
+            console.log('NULL');  
+          } else {  
+            result+= column.value + " ";  
+          }  
+        });  
+        console.log(result);  
+        result ="";  
+    });  
+
+    request.on('done', function(rowCount, more) {  
+    console.log(rowCount + ' rows returned');  
+    });  
+    connection.execSql(request);  
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // create application/json parser
 var jsonParser = bodyParser.json()
