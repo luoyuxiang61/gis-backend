e8b7e94d3a6d8f80c911bc73c089012ebc8d305e
luoyuxiang61 <luoyuxiangqop@gmail.com>
2017-11-28 17:03:31 +0800

    oo

--- index.js
+++ index.js
@@ -1,62 +1,8 @@
+import { setTimeout } from 'timers';
+
 const express = require('express')
 var bodyParser = require('body-parser')
 var app = express();
-var Connection = require('tedious').Connection;
-var config = {
-  userName:'sa',
-  password:'Luoyuxiang61.',
-  server:'localhost',
-  options:{database:'Silverlight_NETDA'} 
-}
-var connection = new Connection(config);
-connection.on('connect',function(err){
-  if(err) console.log('err!');
-  console.log("connected");
-  executeStatement();
-})
-
-var Request = require('tedious').Request;  
-var TYPES = require('tedious').TYPES;  
-
-function executeStatement() {  
-    request = new Request("SELECT * FROM  dbo.BaseDictionary;", function(err) {  
-    if (err) {  
-        console.log(err);}  
-    });  
-    var result = "";  
-    request.on('row', function(columns) {  
-        columns.forEach(function(column) {  
-          if (column.value === null) {  
-            console.log('NULL');  
-          } else {  
-            result+= column.value + " ";  
-          }  
-        });  
-        console.log(result);  
-        result ="";  
-    });  
-
-    request.on('done', function(rowCount, more) {  
-    console.log(rowCount + ' rows returned');  
-    });  
-    connection.execSql(request);  
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 // create application/json parser
@@ -79,6 +25,18 @@ app.post('/',urlencodedParser,function(req,res){
   
   
   res.sendStatus(200);
+})
+
+app.get('/test',urlencodedParser,function(req,res){
+
+
+  
+
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send("test");
+
+
 })
 
 
