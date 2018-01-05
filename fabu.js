953f2e776f5c6d4ed5ee1ccbf42b6f85fc864aa9
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-05 11:54:54 +0800

    eat

--- fabu.js
+++ fabu.js
@@ -395,19 +395,11 @@ app.get('/fields',(req,res) => {
 })
 
 
-
-
-
-
-
-
-
-
 //表格编辑需要的数据源
 app.get('/yesno',function(req,res) {
     res.send(JSON.stringify([
         {value: 1,text: '是'},
-        {value: 0,text: '否'}
+        {value: 0,text: '-'}
     ]))
 })
 
@@ -417,6 +409,16 @@ app.get('/layerType',(req,res) => {
         { value: 1, text: 'TiledService' },
         { value: 2, text: 'FeatureLayer' },
         { value: 3, text: 'GeometryService' },
+    ]))
+})
+
+app.get('/unitName',(req,res) => {
+    res.send(JSON.stringify([
+        { value: 0, text: '米' },
+        { value: 1, text: '千米' },
+        { value: 2, text: '平方米' },
+        { value: 3, text: '平方千米' },
+        { value: 4, text: '' },
     ]))
 })
 
