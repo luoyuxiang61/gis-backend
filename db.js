0e60e629fb30e664e502f1eb9bf28d1b4596a0a0
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-05 17:59:49 +0800

    many to many to be fixed

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const user = require('./model').user
 const group = require('./model').group
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('Silverlight_NETDA', 'sa', 'Luoyuxiang61.', {
+const sequelize = new Sequelize('Fabu', 'sa', 'Luoyuxiang61.', {
   host: 'localhost',
   dialect: 'mssql',
   pool: {
@@ -29,24 +29,40 @@ const BaseLayerField = sequelize.define('BaseLayerField', baseLayerField,
   timestamps: false
 })
 
-// const User = sequelize.define('User', user, {
-//   tableName: 'User',
-//   timestamps: false,
-//   paranoid: true
-// })
+const User = sequelize.define('User', user, {
+  tableName: 'User',
+  timestamps: false,
+  paranoid: true
+})
 
-// const Group = sequelize.define('Group', group, {
-//   tableName: 'Group',
-//   timestamps: false,
-//   paranoid: true
-// })
-// User.belongsTo(Group)
-// Group.hasMany(User)
-// Group.hasMany(BaseMapLayer)
-// BaseMapLayer.hasMany(Group)
-// sequelize.sync({ force: true })
+const Group = sequelize.define('Group', group, {
+  tableName: 'Group',
+  timestamps: false,
+  paranoid: true
+})
+
+
+const UserProject = sequelize.define('user_project', {
+  role: Sequelize.STRING
+});
+User.belongsToMany(Group, { through: UserProject });
+Group.belongsToMany(User, { through: UserProject });
+
+
+
+
+
+
+
+
+User.belongsTo(Group)
+Group.hasMany(User)
+
+
+
+sequelize.sync({ force: true })
 module.exports.bm = BaseMapLayer
 module.exports.bf = BaseLayerField
-// module.exports.user = User
-// module.exports.group = Group
+module.exports.user = User
+module.exports.group = Group
 module.exports.sql = sequelize
