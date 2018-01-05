3b864edc152967bf72268099b33d151e44a2c66d
luoyuxiang61 <luoyuxiangqop@gmail.com>
2018-01-05 17:23:45 +0800

    new start

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const user = require('./model').user
 const group = require('./model').group
 
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('Fabu', 'sa', 'Luoyuxiang61.', {
+const sequelize = new Sequelize('Silverlight_NETDA', 'sa', 'Luoyuxiang61.', {
   host: 'localhost',
   dialect: 'mssql',
   pool: {
@@ -29,24 +29,24 @@ const BaseLayerField = sequelize.define('BaseLayerField', baseLayerField,
   timestamps: false
 })
 
-const User = sequelize.define('User', user, {
-  tableName: 'User',
-  timestamps: false,
-  paranoid: true
-})
+// const User = sequelize.define('User', user, {
+//   tableName: 'User',
+//   timestamps: false,
+//   paranoid: true
+// })
 
-const Group = sequelize.define('Group', group, {
-  tableName: 'Group',
-  timestamps: false,
-  paranoid: true
-})
-User.belongsTo(Group)
-Group.hasMany(User)
-Group.hasMany(BaseMapLayer)
-BaseMapLayer.hasMany(Group)
-sequelize.sync({ force: true })
+// const Group = sequelize.define('Group', group, {
+//   tableName: 'Group',
+//   timestamps: false,
+//   paranoid: true
+// })
+// User.belongsTo(Group)
+// Group.hasMany(User)
+// Group.hasMany(BaseMapLayer)
+// BaseMapLayer.hasMany(Group)
+// sequelize.sync({ force: true })
 module.exports.bm = BaseMapLayer
 module.exports.bf = BaseLayerField
-module.exports.user = User
-module.exports.group = Group
+// module.exports.user = User
+// module.exports.group = Group
 module.exports.sql = sequelize
